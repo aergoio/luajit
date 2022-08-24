dd8a3b341301bdf459ac4c0f2f3b3d3031eadc6a
Sung-Jae Woo <qcollapse53@gmail.com>
2022-08-24 16:31:29 +0900

    Make enable gas volatile

--- src/lj_dispatch.h
+++ src/lj_dispatch.h
@@ -98,7 +98,7 @@ typedef struct GG_State {
 #endif
   ASMFunction dispatch[GG_LEN_DISP];	/* Instruction dispatch tables. */
   BCIns bcff[GG_NUM_ASMFF];		/* Bytecode for ASM fast functions. */
-  uint8_t enable_gas;
+  volatile uint8_t enable_gas;
   volatile uint64_t total_gas;
   uint16_t gas[BC__MAX];
 } GG_State;
